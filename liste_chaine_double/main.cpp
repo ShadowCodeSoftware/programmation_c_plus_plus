fbfd4b86a5d3abe7d22ddfff839653639306b987
HASSANA DAOUDA Daouda <daoudahassan@gmail.com>
2025-12-18 09:39:16 +0100

    AJout des supprsessoins (suppTecte, suppMiilieu et SuppQueue)

--- liste_chaine_double/main.cpp
+++ liste_chaine_double/main.cpp
@@ -62,6 +62,101 @@ void ajoutQueue(Liste*& head, int newData) {
     }
 }
 
+// Fonction pour supprimer un élément en tête de liste
+void suppressionTete(Liste*& head) {
+    if (head == nullptr) {
+        cerr << "La liste est vide" << endl;
+        return;
+    }
+
+    Liste* temp = head;
+    head = head->next;
+    
+    if (head != nullptr) {
+        head->prev = nullptr;
+    }
+    
+    delete temp;
+}
+
+// Fonction pour supprimer un élément au milieu de la liste
+void suppressionMilieu(Liste*& head, int position) {
+    if (position < 1 || head == nullptr) {
+        cerr << "Position invalide" << endl;
+        return;
+    }
+
+    if (position == 1) {
+        suppressionTete(head);
+    } else {
+        Liste* courant = head;
+        int currentPosition = 1;
+
+        while (courant != nullptr && currentPosition < position) {
+            courant = courant->next;
+            currentPosition++;
+        }
+
+        if (courant != nullptr) {
+            if (courant->prev != nullptr) {
+                courant->prev->next = courant->next;
+            }
+            if (courant->next != nullptr) {
+                courant->next->prev = courant->prev;
+            }
+            delete courant;
+        } else {
+            cerr << "Position invalide" << endl;
+        }
+    }
+}
+
+// Fonction pour supprimer un élément en queue de liste
+void suppressionQueue(Liste*& head) {
+    if (head == nullptr) {
+        cerr << "La liste est vide" << endl;
+        return;
+    }
+
+    if (head->next == nullptr) {
+        delete head;
+        head = nullptr;
+    } else {
+        Liste* courant = head;
+        while (courant->next != nullptr) {
+            courant = courant->next;
+        }
+        
+        if (courant->prev != nullptr) {
+            courant->prev->next = nullptr;
+        }
+        delete courant;
+    }
+}
+
+// Fonction pour obtenir la longueur de la liste
+int longueurListe(Liste* head) {
+    int longueur = 0;
+    Liste* courant = head;
+    while (courant != nullptr) {
+        longueur++;
+        courant = courant->next;
+    }
+    return longueur;
+}
+
+// Fonction pour rechercher un élément
+bool rechercheElement(Liste* head, int valeur) {
+    Liste* courant = head;
+    while (courant != nullptr) {
+        if (courant->elt == valeur) {
+            return true;
+        }
+        courant = courant->next;
+    }
+    return false;
+}
+
 // Fonction pour afficher la liste doublement chaînée
 void afficherListe(Liste* head) {
     Liste* courant = head;
@@ -110,6 +205,42 @@ int main() {
     afficherListe(tete);
 
     cout << endl;
+
+    // Affichage de la longueur
+    cout << "Longueur de la liste : " << longueurListe(tete) << endl << endl;
+
+    // Recherche d'éléments
+    if (rechercheElement(tete, 10)) {
+        cout << "L'element 10 est trouve dans la liste" << endl;
+    } else {
+        cout << "L'element 10 n'est pas trouve" << endl;
+    }
+
+    if (rechercheElement(tete, 100)) {
+        cout << "L'element 100 est trouve dans la liste" << endl;
+    } else {
+        cout << "L'element 100 n'est pas trouve" << endl;
+    }
+
+    cout << endl;
+
+    // Suppression en tête
+    suppressionTete(tete);
+    cout << "Apres suppression en tete : ";
+    afficherListe(tete);
+    cout << "Longueur : " << longueurListe(tete) << endl << endl;
+
+    // Suppression en queue
+    suppressionQueue(tete);
+    cout << "Apres suppression en queue : ";
+    afficherListe(tete);
+    cout << "Longueur : " << longueurListe(tete) << endl << endl;
+
+    // Suppression au milieu (position 2)
+    suppressionMilieu(tete, 2);
+    cout << "Apres suppression a la position 2 : ";
+    afficherListe(tete);
+    cout << "Longueur : " << longueurListe(tete) << endl << endl;
 
     // Libération de la mémoire
     while (tete != nullptr) {
